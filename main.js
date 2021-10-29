4508445f9593cd7208b7e96443c0068793b916a5
Eduardo Neves <62307330+snowedz@users.noreply.github.com>
2021-10-29 10:09:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function changeName() {
+  document.getElementById('userName').textContent = "Snow"
+}
+
+changeName()
+
+const linksSocialMedia ={
+  youtube: "shunpiske",
+  facebook: "snowedz",
+  instagram: "snowedz",
+  twitter: "tylensnow",
+  github: "snowedz"
+}
+
+function changeSocialLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialLinks()
+
+function getGitHubData() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}` 
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => { 
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userUrl.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+  
+} 
+getGitHubData()
